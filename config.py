87f6ffed7401e67640f08d09352c3f37dddb12b3
Jueri <jueri.keller@smail.th-koeln.de>
2021-12-27 21:34:37 +0000

    add example notebook to explain the module

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+import nltk
+import subprocess
+import spacy
+
+DEBUG: bool = False
+
+
+SAMPLE_PDF_PATH = os.path.join(
+    "data", "pdfs", "Transkript_Quantencomputer_SMC-Press-Briefing_2021-04-12.pdf"
+)
+
+
+# NLTK
+NLTK_DATA_PATH = os.path.join("data", "nltk_data")
+
+# setup NLTK
+if not os.path.exists(NLTK_DATA_PATH):
+    os.makedirs(NLTK_DATA_PATH)
+    # nltk.download("vader_lexicon", NLTK_DATA_PATH)
+    # nltk.download("wordnet", NLTK_DATA_PATH)
+    # nltk.download("stopwords", NLTK_DATA_PATH)
+    nltk.download("punkt", NLTK_DATA_PATH)
+nltk.data.path.append(NLTK_DATA_PATH)
+
+
+# Spacy
+SPACY_MODEL_NAME = "de_core_news_lg"
+SPACY_DATA_PATH = os.path.join("data", "spacy_data", SPACY_MODEL_NAME)
+
+if not os.path.exists(SPACY_DATA_PATH):
+    subprocess.run(["python", "-m", "spacy", "download", SPACY_MODEL_NAME])  # download model
+    os.makedirs(SPACY_DATA_PATH)  # create foleder
+    nlp = spacy.load(SPACY_MODEL_NAME)  # load model
+    nlp.to_disk(SPACY_DATA_PATH)  # save model to created dir
